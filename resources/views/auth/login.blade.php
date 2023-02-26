d0a761f76fe001c0275acaeb061499374bf97da8
encrypthink <wijay11ipa@gmail.com>
2023-02-26 18:31:01 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+
         </x-slot>
 
         <!-- Session Status -->
@@ -14,6 +12,11 @@
 
             <!-- Email Address -->
             <div>
+                <div class="flex justify-center">
+                    <a href="/">
+                        <img src="{{ asset('images/logo.png') }}" class="w-32" alt="">
+                    </a>
+                </div>
                 <x-input-label for="email" :value="__('Email')" />
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
